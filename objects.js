cd823ce1c32c009b39dc0b0d4f7b6ed9c178b6f8
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-23 02:56:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,5 +5,6 @@ var playlist = {
 }
 
 function updatePlaylist() {
-    playlist.'Phil Ochs' = 'Here\'s to the State of Mississippi';
-  }
+  return Object.assign({}, playlist, { ["Phil Ochs"]: "Here\'s to the State of Mississippi" })
+}
+  
